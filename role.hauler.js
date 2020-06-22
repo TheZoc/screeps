b5e7dfb2666ee4e92f38efd51323db9f0737e56e
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2020-06-22 17:26:41 +0100

    Improved hauler behavior

--- role.hauler.js
+++ role.hauler.js
@@ -1,54 +1,50 @@
 var roleHauler = {
 
-    /** @param {Creep} creep **/
+    /**
+     * @param {Creep} creep 
+     */
     run: function(creep)
     {
-        const targetPos = new RoomPosition(creep.room.memory.sources[creep.memory.source].x,
-                                           creep.room.memory.sources[creep.memory.source].y,
-                                           creep.room.name);
-
-        let structures = targetPos.lookFor(LOOK_STRUCTURES);
-
-        if (!structures.length)
+        if(creep.memory.withdraw == true || creep.memory.withdraw == undefined)
         {
-            creep.say('🛑PANIC🛑');
-            return;
-        }
+            if (creep.memory.fromStructure == undefined)
+            {
+                const foundStructure = this.pick_resource_target(creep);
+                if (!foundStructure)
+                {
+                    // Panic! Log it.
+                    console.log("Hauler Panic!")
+                    return;
+                }
+            }
 
-        if (structures[0].structureType != 'container')
-        {
-            creep.say('⚠️PANIC⚠️');
-            return;
-        }
-
-        if(!creep.memory.withdraw && creep.carry.energy == 0)
-        {
-            creep.say('🔼withdraw');
-            creep.memory.withdraw = true;
-        }
-        if(creep.memory.withdraw && creep.carry.energy == creep.carryCapacity)
-        {
-            creep.say('🔽deposit');
-            creep.memory.withdraw = false;
-        }
-
-        if(creep.memory.withdraw)
-        {
-            const withdrawResult = creep.withdraw(structures[0], RESOURCE_ENERGY);
+            const withdrawResult = creep.withdraw(Game.getObjectById(creep.memory.fromStructure), RESOURCE_ENERGY);
             if(withdrawResult == ERR_NOT_IN_RANGE)
             {
-                creep.moveTo(structures[0], {visualizePathStyle: {stroke: '#FFEA88'}});
+                creep.moveTo(Game.getObjectById(creep.memory.fromStructure), {visualizePathStyle: {stroke: '#FFEA88'}});
             }
-            // HACK HACK HACK - Lets speed things up if there's not enough energy to be withdrawed
-            else if (withdrawResult == OK && creep.carry.energy > 0 && creep.carry.energy < creep.carryCapacity)
+            else if (withdrawResult == ERR_NOT_ENOUGH_RESOURCES)
             {
+                this.pick_resource_target(creep, true); // Skip storage
+            }
+            else if (withdrawResult == OK)
+            {
+                // HACK HACK HACK - Lets speed things up if there's not enough energy to be withdrawed
+                if (creep.carry.energy > 0 && creep.carry.energy < creep.carryCapacity)
+                {
+                    creep.say('🔽early d');                
+                }
+                else
+                {
+                    creep.say('🔽deposit');
+                }
                 creep.memory.withdraw = false;
-                creep.say('🔽early d');                
+                delete creep.memory["fromStructure"];
             }
         }
         else
         {
-            let targets = creep.room.find(FIND_MY_STRUCTURES,
+            const targets = creep.room.find(FIND_MY_STRUCTURES,
             {
                 filter: (structure) =>
                 {
@@ -61,20 +57,21 @@ var roleHauler = {
             if(targets.length > 0)
             {
                 // TODO: Should we remove towers from this selection?
-                let target = creep.pos.findClosestByPath(targets);
-
-//                if (creep.room.name != 'W2N5')
-//                    console.log(creep.room.name + ' - ' + target);
-
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                const target = creep.pos.findClosestByPath(targets);
+                const transferResult = creep.transfer(target, RESOURCE_ENERGY);
+                if(transferResult == ERR_NOT_IN_RANGE)
                 {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+                else if (transferResult == OK)
+                {
+                    creep.memory.withdraw = true;
                 }
             }
             else
             {
                 // If we're here, all the spawns, extensions and structures are at their capacity. Start moving things to our storage.
-                let storages = creep.room.find(FIND_MY_STRUCTURES,
+                const storages = creep.room.find(FIND_MY_STRUCTURES,
                 {
                     filter: (structure) =>
                     {
@@ -84,21 +81,82 @@ var roleHauler = {
 
                 if (storages.length > 0)
                 {
-                    let target = creep.pos.findClosestByPath(storages);
-                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                    const target = creep.pos.findClosestByPath(storages);
+                    const transferResult = creep.transfer(target, RESOURCE_ENERGY);
+                    if(transferResult == ERR_NOT_IN_RANGE)
                     {
                         creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                    else if (transferResult == OK)
+                    {
+                        creep.memory.withdraw = true;
                     }
                 }
                 else
                 {
                     // No loitering! - Get out of the way so others can pass.
-                    let target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+                    const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
 
             }
         }
+    }, 
+
+    pick_resource_target: function(creep, forceContainer = false)
+    {
+        if (!forceContainer)
+        {
+            // This initial block check if there's less than 50% of resources available in spawn + extensions.
+            // If so, try to fetch from the room storage, if available.
+            let foundResourceSource = false;
+            let maxResources = 0;
+            let currentResource = 0;
+            const resourceStructures = creep.room.find(FIND_MY_STRUCTURES,
+            {
+                filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
+            });
+
+            for (const s in resourceStructures)
+            {
+                currentResource += resourceStructures[s].store.getUsedCapacity(RESOURCE_ENERGY);
+                maxResources    += resourceStructures[s].store.getCapacity(RESOURCE_ENERGY);
+            }
+
+            // Less than 60% of the total (spawn + extensions) available? Check if there's a storage and get it from there. ("High availability" mode)
+            if (currentResource < maxResources * 0.6 && creep.room.storage != undefined)
+            {
+                // Determine current state
+                if (creep.room.storage.store.getUsedCapacity(RESOURCE_ENERGY) >= creep.carryCapacity)
+                {
+                    creep.say('🔼withdraw');
+                    creep.memory.withdraw = true;
+                    creep.memory.fromStructure = creep.room.storage.id;
+                    return true;
+                }
+            }
+        }
+
+        // Still here? Find the source container this creeps is assigned to.
+        const targetPos = new RoomPosition(creep.room.memory.sources[creep.memory.source].x,
+                                           creep.room.memory.sources[creep.memory.source].y,
+                                           creep.room.name);
+
+        const structures = targetPos.lookFor(LOOK_STRUCTURES);
+
+        if (!structures.length)
+        {
+            creep.say('🛑PANIC🛑');
+            return false;
+        }
+
+        if (structures[0].structureType != 'container')
+        {
+            creep.say('⚠️PANIC⚠️');
+            return false;
+        }
+        creep.memory.fromStructure = structures[0].id;
+        return true;
     }
 
 };
