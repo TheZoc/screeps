07c4f1dadb7fc08ab9c21824df14692b4699a4c7
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2020-06-25 12:42:12 +0100

    Added some documentation to util.js

--- util.js
+++ util.js
@@ -1,24 +1,35 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
+//////////////////////////////////////////////////////////////////////////////
+// Utility functions that used through the code
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A function that, given an array of structures, returns the one with the most energy stored
  *
- * You can import it from another modules like this:
- * var mod = require('util');
- * mod.thing == 'a thing'; // true
+ * @param {array<Structure>>} structures - An array of structures to be tested
+ * @param {string}            resource   - The type of resource to check, one of the RESOURCE_* constants. If omitted, defaults to RESOURCE_ENERGY
+ *
+ * @returns {null|*} - Returns the structure with most resources, or null if the function wasn't able to find a valid structure.
  */
-
-function maxRes(c, res = RESOURCE_ENERGY) { // You can refactor this part
-    if(!c || !c.length) // Safety check to prevent Infinity
+function maxRes(structures, resource = RESOURCE_ENERGY)
+{
+    if(!structures || !structures.length) // Safety check to prevent Infinity
         return null;
-    return _.max(c, c => c.store[res] || 0);
+    return _.max(structures, s => s.store[resource] || 0);
 }
 
-function minRes(c, res = RESOURCE_ENERGY) { // You can refactor this part
-    if(!c || !c.length) // Safety check to prevent Infinity
+/**
+ * A function that, given an array of structures, returns the one with the least energy stored
+ *
+ * @param {array<Structure>>} structures - An array of structures to be tested
+ * @param {string}            resource   - The type of resource to check, one of the RESOURCE_* constants. If omitted, defaults to RESOURCE_ENERGY
+ *
+ * @returns {null|*} - Returns the structure with less resources, or null if the function wasn't able to find a valid structure.
+ */
+function minRes(structures, resource = RESOURCE_ENERGY) { // You can refactor this part
+    if(!structures || !structures.length) // Safety check to prevent Infinity
         return null;
-    return _.min(c, c => c.store[res] || 0);
+    return _.min(structures, s => s.store[resource] || 0);
 }
-
 
 /**
  * Display message and spawn unit if requirements are met
@@ -29,6 +40,8 @@ function minRes(c, res = RESOURCE_ENERGY) { // You can refactor this part
  * @param {dict} [memory]               - (Optional) The dict with the data to be inserted into the creep's memory
  * @param {string} [prettyName]         - (Optional) Pretty creep name to be displayed. Name will be used if this is missing.
  * @param {int} [offsetY]               - (Optional) Offset for Y coordinate, for message position
+ *
+ * @return {OK|number|ERR_NOT_OWNER|ERR_NAME_EXISTS|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_ARGS|ERR_RCL_NOT_ENOUGH}
  */
 function spawn(targetSpawn, bodyParts, name, memory, prettyName, offsetY)
 {
