e65cc3e0ab88038655ceca6d4852513d8d6a7b8f
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2020-06-25 14:16:00 +0100

    More documentation

--- logic.tower.js
+++ logic.tower.js
@@ -1,5 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+// Tower logic
+//////////////////////////////////////////////////////////////////////////////
+
 var logicTower =
 {
+    /**
+     * Runs the logic for the towers in a given room.
+     *
+     * @param {Room} room - Target room to process all the towers
+     */
     run: function(room)
     {
         let towers = (room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_TOWER }}));
@@ -7,6 +16,7 @@ var logicTower =
         {
             for(let i = 0, l = towers.length; i < l; ++i)
             {
+                // Top priority: Attack hostile creeps
                 let closestHostile = towers[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 if(closestHostile)
                 {
@@ -14,24 +24,26 @@ var logicTower =
                 }
                 else
                 {
-                    let closestDamagedStructure = towers[i].pos.findClosestByRange(FIND_MY_STRUCTURES, {
+                    // No threat? Repair damaged structures
+                    let closestOwnedDamagedStructure = towers[i].pos.findClosestByRange(FIND_MY_STRUCTURES, {
                         filter: (structure) => structure.hits < structure.hitsMax
                     });
 
-                    if(closestDamagedStructure)
+                    if(closestOwnedDamagedStructure)
                     {
-                        towers[i].repair(closestDamagedStructure);
+                        towers[i].repair(closestOwnedDamagedStructure);
                     }
                     else
                     {
-                        let closestContainer = towers[i].pos.findClosestByRange(FIND_STRUCTURES, {
-                            filter: (structure) => (structure.structureType == STRUCTURE_CONTAINER ||
-                                                    structure.structureType == STRUCTURE_ROAD) && structure.hits < structure.hitsMax
+                        // We also want to repair roads and containers in our maps
+                        let closestDamagedStructure = towers[i].pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => (structure.structureType === STRUCTURE_CONTAINER ||
+                                                    structure.structureType === STRUCTURE_ROAD) && structure.hits < structure.hitsMax
                         });
 
-                        if(closestContainer)
+                        if(closestDamagedStructure)
                         {
-                            towers[i].repair(closestContainer);
+                            towers[i].repair(closestDamagedStructure);
                         }
                     }
                 }
