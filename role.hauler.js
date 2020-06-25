82b44005c9743472dab54f898e4da5873415806e
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2020-06-25 15:13:02 +0100

    Added documentation for `role.haules.js` and improved its behavior when trying to collect from an empty storage.

--- role.hauler.js
+++ role.hauler.js
@@ -1,13 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+// This is a role for the Hauler creep.
+// Their purpose is to fetch the resources harvested by the Static Harvester
+// from its container and transfer it to the closest tower, spawn or
+// extension. When they're full, start stocking the resources in the room's
+// storage.
+//////////////////////////////////////////////////////////////////////////////
+
 var roleHauler = {
 
     /**
-     * @param {Creep} creep 
+     * This function runs the Hauler behavior for the given creep.
+     * It is a finite state machine with 2 possible states: Withdrawing or not.
+     *
+     * If the creep has just spawned, it will look for a resource pool and then
+     * it will continue in the withdraw state.
+     *
+     * Withdraw state:
+     * Move to the target resource pool and collect resources.
+     * If a transfer (collect) action had resources, but not enough to fill the
+     * creep store capacity, the creep does an early deliver, instead of waiting
+     * for the resources to be available.
+     *
+     * Deliver (non-withdraw) state:
+     * Go to the nearest Tower, Spawn or Extension that needs resources, and
+     * transfer them there. Do this until there's no more resources being carried.
+     * (This logic definitely can be improved with priorities!)
+     * If every Spawn, Container and Extension are filled, transfer to our storage.
+     * If storage is full, move to the nearest Spawn and idle there.
+     *
+     * @param {Creep} creep - Creep to execute the role
      */
     run: function(creep)
     {
-        if(creep.memory.withdraw == true || creep.memory.withdraw == undefined)
+        if(creep.memory.withdraw === true || creep.memory.withdraw === undefined)
         {
-            if (creep.memory.fromStructure == undefined)
+            if (creep.memory.fromStructure === undefined)
             {
                 const foundStructure = this.pick_resource_target(creep);
                 if (!foundStructure)
@@ -18,21 +45,24 @@ var roleHauler = {
                 }
             }
 
-            const withdrawResult = creep.withdraw(Game.getObjectById(creep.memory.fromStructure), RESOURCE_ENERGY);
-            if(withdrawResult == ERR_NOT_IN_RANGE)
+            const withdrawSource = Game.getObjectById(creep.memory.fromStructure);
+            const withdrawResult = creep.withdraw(withdrawSource, RESOURCE_ENERGY);
+            if(withdrawResult === ERR_NOT_IN_RANGE)
             {
                 creep.moveTo(Game.getObjectById(creep.memory.fromStructure), {visualizePathStyle: {stroke: '#FFEA88'}});
             }
-            else if (withdrawResult == ERR_NOT_ENOUGH_RESOURCES)
+            else if (withdrawResult === ERR_NOT_ENOUGH_RESOURCES && withdrawSource.id === creep.room.storage.id)
             {
-                this.pick_resource_target(creep, true); // Skip storage
+                // If we were fetching from storage and it's now empty, get resources from somewhere else.
+                this.pick_resource_target(creep, true);
             }
-            else if (withdrawResult == OK)
+            else if (withdrawResult === OK)
             {
-                // HACK HACK HACK - Lets speed things up if there's not enough energy to be withdrawed
-                if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.store.getUsedCapacity(RESOURCE_ENERGY) < creep.store.getCapacity(RESOURCE_ENERGY))
+                // HACK HACK HACK - Lets speed things up if there's not enough energy to be withdrawn
+                if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+                    creep.store.getUsedCapacity(RESOURCE_ENERGY) < creep.store.getCapacity(RESOURCE_ENERGY))
                 {
-                    creep.say('🔽early d');                
+                    creep.say('🔽early d');
                 }
                 else
                 {
@@ -48,9 +78,9 @@ var roleHauler = {
             {
                 filter: (structure) =>
                 {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    return (structure.structureType === STRUCTURE_EXTENSION ||
+                            structure.structureType === STRUCTURE_SPAWN ||
+                            structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
 
@@ -59,7 +89,7 @@ var roleHauler = {
                 // TODO: Should we remove towers from this selection?
                 const target = creep.pos.findClosestByPath(targets);
                 const transferResult = creep.transfer(target, RESOURCE_ENERGY);
-                if(transferResult == ERR_NOT_IN_RANGE)
+                if(transferResult === ERR_NOT_IN_RANGE)
                 {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
@@ -76,7 +106,7 @@ var roleHauler = {
                 {
                     filter: (structure) =>
                     {
-                        return structure.structureType == STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                        return structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
                 });
 
@@ -84,7 +114,7 @@ var roleHauler = {
                 {
                     const target = creep.pos.findClosestByPath(storages);
                     const transferResult = creep.transfer(target, RESOURCE_ENERGY);
-                    if(transferResult == ERR_NOT_IN_RANGE)
+                    if(transferResult === ERR_NOT_IN_RANGE)
                     {
                         creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
@@ -100,23 +130,36 @@ var roleHauler = {
                     const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-
             }
         }
-    }, 
+    },
 
-    pick_resource_target: function(creep, forceContainer = false)
+    /**
+     * This function selects a new resource pool, for the creep, to collect resources from.
+     *
+     * If `skipStorage` is false (default), it will check if the room and spawns have less
+     * than 60% resources filled and, if that's the case, will prioritize collecting resources
+     * from the Storage, to speed things up.
+     *
+     * Otherwise, this creep will move to their assigned container and collect from there.
+     *
+     * @param {Creep} creep         - Target creep that is looking for a new resource pool.
+     * @param {boolean} skipStorage - Ignore the Storage as a valid resource pool.
+     *
+     * @return {boolean}
+     */
+    pick_resource_target: function(creep, skipStorage = false)
     {
-        if (!forceContainer)
+        if (!skipStorage)
         {
             // This initial block check if there's less than 50% of resources available in spawn + extensions.
             // If so, try to fetch from the room storage, if available.
-            let foundResourceSource = false;
             let maxResources = 0;
             let currentResource = 0;
             const resourceStructures = creep.room.find(FIND_MY_STRUCTURES,
             {
-                filter: (structure) => (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
+                filter: (structure) => (structure.structureType === STRUCTURE_EXTENSION ||
+                                        structure.structureType === STRUCTURE_SPAWN)
             });
 
             for (const s in resourceStructures)
@@ -126,7 +169,7 @@ var roleHauler = {
             }
 
             // Less than 60% of the total (spawn + extensions) available? Check if there's a storage and get it from there. ("High availability" mode)
-            if (currentResource < maxResources * 0.6 && creep.room.storage != undefined)
+            if (currentResource < maxResources * 0.6 && creep.room.storage !== undefined)
             {
                 // Determine current state
                 if (creep.room.storage.store.getUsedCapacity(RESOURCE_ENERGY) >= creep.carryCapacity)
@@ -152,7 +195,7 @@ var roleHauler = {
             return false;
         }
 
-        if (structures[0].structureType != 'container')
+        if (structures[0].structureType !== 'container')
         {
             creep.say('⚠️PANIC⚠️');
             return false;
