1fdc4d6b179538326f3df2806e2eb838fc951837
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2020-06-25 16:22:10 +0100

    `util.roadplanner.js` cleanup. (It's still WIP, anyway)

--- util.roadplanner.js
+++ util.roadplanner.js
@@ -4,36 +4,36 @@ var utilRoadPlanner =
     drawColor: '#f00',
 
     /**
-     * Draw the path stored in `pathfinderResults`
+     * Draw the path stored in `this.pathfinderResults`
      */
     draw_planned_road: function()
     {
-        if (!pathfinderResults)
+        if (!this.pathfinderResults)
             return;
 
-        for(var i = 0, l = pathfinderResults.path.length - 1; i < l; ++i)
+        for(var i = 0, l = this.pathfinderResults.path.length - 1; i < l; ++i)
         {
-            if (Game.rooms[pathfinderResults.path[i].roomName]   != undefined &&
-                Game.rooms[pathfinderResults.path[i+1].roomName] != undefined)
+            if (Game.rooms[this.pathfinderResults.path[i].roomName]   !== undefined &&
+                Game.rooms[this.pathfinderResults.path[i+1].roomName] !== undefined)
             {
-                if (pathfinderResults.path[i].roomName != pathfinderResults.path[i+1].roomName)
+                if (this.pathfinderResults.path[i].roomName !== this.pathfinderResults.path[i+1].roomName)
                     continue;
 
-                Game.rooms[pathfinderResults.path[i].roomName].visual.line(pathfinderResults.path[i],
-                                                                           pathfinderResults.path[i+1],
-                                                                           {width: 0.15, color: drawColor, opacity: 0.8, lineStyle: 'undefined'});
+                Game.rooms[this.pathfinderResults.path[i].roomName].visual.line(this.pathfinderResults.path[i],
+                                                                           this.pathfinderResults.path[i+1],
+                                                                           {width: 0.15, color: this.drawColor, opacity: 0.8, lineStyle: 'undefined'});
             }
         }
 /*
-        for(var i = 0, l = pathfinderResults.path.length; i < l; ++i)
+        for(var i = 0, l = this.pathfinderResults.path.length; i < l; ++i)
         {
-            if (Game.rooms[pathfinderResults.path[i].roomName] != undefined)
+            if (Game.rooms[this.pathfinderResults.path[i].roomName] != undefined)
             {
-                Game.rooms[pathfinderResults.path[i].roomName].visual.rect(pathfinderResults.path[i].x - 0.5,
-                                                                           pathfinderResults.path[i].y - 0.5,
+                Game.rooms[this.pathfinderResults.path[i].roomName].visual.rect(this.pathfinderResults.path[i].x - 0.5,
+                                                                           this.pathfinderResults.path[i].y - 0.5,
                                                                            1,
                                                                            1,
-                                                                           {fill: drawColor, stroke: 'transparent', strokeWidth: '0.05', opacity: '0.4'});
+                                                                           {fill: this.drawColor, stroke: 'transparent', strokeWidth: '0.05', opacity: '0.4'});
             }
         }
 //*/
@@ -42,16 +42,16 @@ var utilRoadPlanner =
 
     /**
      * Find the optimal path between origin and destination, ignoring creeps.
-     * This will store a path on `pathfinderResults` starting on a neighbor cell
+     * This will store a path on `this.pathfinderResults` starting on a neighbor cell
      * from origin and ending in a neighbor cell in destination.
      *
-     * @param {origin} RoomPosition       - The initial coordinate for the search
-     * @param {destination} RoomPosition  - The final coordinate for the search
+     * @param {RoomPosition} origin       - The initial coordinate for the search
+     * @param {RoomPosition} destination  - The final coordinate for the search
      */
     plan: function(origin, destination)
     {
-        drawColor = '#f00';
-        pathfinderResults = PathFinder.search(
+        this.drawColor = '#f00';
+        this.pathfinderResults = PathFinder.search(
             origin,                         // TODO: Get from memory
             { pos: destination, range:1 },  // Game.spawns['Spawn1'].pos
             {
@@ -92,28 +92,28 @@ var utilRoadPlanner =
     },
 
     /**
-     * Create the construction sites for the path stored in pathfinderResults
+     * Create the construction sites for the path stored in this.pathfinderResults
      */
     build_roads()
     {
-        drawColor = '#BEFFE0';
-        for(var i = 0, l = pathfinderResults.path.length; i < l; ++i)
+        this.drawColor = '#BEFFE0';
+        for(var i = 0, l = this.pathfinderResults.path.length; i < l; ++i)
         {
             // TODO: If it is possible to have multiple structures in a single spot, update here.
-            let struct = pathfinderResults.path[i].lookFor(LOOK_STRUCTURES);
+            let struct = this.pathfinderResults.path[i].lookFor(LOOK_STRUCTURES);
 
-            if (struct.length && struct[0].structureType == 'road')
+            if (struct.length && struct[0].structureType === 'road')
                 continue;
 
             if (struct.length > 1)
-                console.log('Found more than one structure at (' + pathfinderResults.path[i].x + ', ' + pathfinderResults.path[i].y + '). Need to investigate.');
+                console.log('Found more than one structure at (' + this.pathfinderResults.path[i].x + ', ' + this.pathfinderResults.path[i].y + '). Need to investigate.');
 
-            let cs = pathfinderResults.path[i].lookFor(LOOK_CONSTRUCTION_SITES);
+            let cs = this.pathfinderResults.path[i].lookFor(LOOK_CONSTRUCTION_SITES);
 
-            if (cs.length && cs[0].structureType == 'road')
+            if (cs.length && cs[0].structureType === 'road')
                 continue;
 
-            pathfinderResults.path[i].createConstructionSite(STRUCTURE_ROAD);
+            this.pathfinderResults.path[i].createConstructionSite(STRUCTURE_ROAD);
         }
     },
 };
