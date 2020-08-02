TheZoc/screeps

commit 85cf5fa0362ba6f8721ce004eefa8064bfb05ed2

- Added resource selling code! - Added hacky hauler logic to fill the terminal with up to 2000 energy.

role.hauler.js
@@ -173,36 +173,59 @@ var roleHauler = {
             }
             else
             {
-                // If we're here, all the spawns, extensions and structures are at their capacity. Start moving things to our storage.
-                const storages = creep.room.find(FIND_MY_STRUCTURES,
+                // Quick hack to store up to 2000 energy in the terminal.
+                // TODO: Improve this mess! :(
+                if (creep.room.terminal !== undefined)
                 {
-                    filter: (structure) =>
+                    if (creep.room.terminal.store.getUsedCapacity(RESOURCE_ENERGY) < 2000)
                     {
-                        return structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
+                        const target = creep.room.terminal;
+                        const transferResult = creep.transfer(target, RESOURCE_ENERGY);
+                        if(transferResult === ERR_NOT_IN_RANGE)
+                        {
+                            if (!creep.fatigue)
+                                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                        }
 
-                if (storages.length > 0)
-                {
-                    const target = creep.pos.findClosestByPath(storages);
-                    const transferResult = creep.transfer(target, RESOURCE_ENERGY);
-                    if(transferResult === ERR_NOT_IN_RANGE)
-                    {
-                        if (!creep.fatigue)
-                            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-
-                    if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0)
-                    {
-                        creep.memory.withdraw = true;
+                        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0)
+                        {
+                            creep.memory.withdraw = true;
+                        }
                     }
                 }
                 else
                 {
-                    // No loitering! - Get out of the way so others can pass.
-                    const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-                    if (!creep.fatigue)
-                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    // If we're here, all the spawns, extensions and structures are at their capacity. Start moving things to our storage.
+                    const storages = creep.room.find(FIND_MY_STRUCTURES,
+                        {
+                            filter: (structure) =>
+                            {
+                                return structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                            }
+                        });
+
+                    if (storages.length > 0)
+                    {
+                        const target = creep.pos.findClosestByPath(storages);
+                        const transferResult = creep.transfer(target, RESOURCE_ENERGY);
+                        if(transferResult === ERR_NOT_IN_RANGE)
+                        {
+                            if (!creep.fatigue)
+                                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                        }
+
+                        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0)
+                        {
+                            creep.memory.withdraw = true;
+                        }
+                    }
+                    else
+                    {
+                        // No loitering! - Get out of the way so others can pass.
+                        const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+                        if (!creep.fatigue)
+                            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }
             }
         }

logic.market.js
@@ -47,13 +47,22 @@ let logicMarket = {
         const bestOrders = this.getBestThreeBuyOrders(room, actualResourcesToSell);
         console.log('Best orders:\n' + ex(bestOrders));
 
-        console.log("Here the good would be sold, but the code is disabled for now until we're happy with the formulas for estimating profits");
+        //console.log("Here the good would be sold, but the code is disabled for now until we're happy with the formulas for estimating profits");
 
         // SELL!
-        // TODO: check the amount of available resources vs the amount of requested resources in the buy order.
-        // let result = Game.market.deal(bestOrders[0].id, 1000, room.name);
-        // if (result === 0)
-        //     console.log('Order completed successfully');
+        const amountToSell = Math.min(room.terminal.store[bestOrders[0].resourceType], bestOrders[0].remainingAmount,  bestOrders[0].amount);
+        let result = Game.market.deal(bestOrders[0].id, amountToSell, room.name);
+        if (result === 0)
+        {
+            let buffer = 'Order completed successfully:\n' +
+                'Resource: ' + bestOrders[0].resourceType + '\n' +
+                'Amount: ' + amountToSell + '\n' +
+                'Credits ' + amountToSell * bestOrders[0].price + '\n' +
+                'Order details:\n' + ex(bestOrders[0]);
+
+            console.log(buffer);
+            Game.notify(buffer, 360);
+        }
     },
 
     /**

commit 8b8867ef6094ae135f2b902447f101e7f51cd305

- Fixed an issue with the transporter getting stuck, introduced in 85cf5fa0362ba6f8721ce004eefa8064bfb05ed2

role.hauler.js
@@ -191,41 +191,40 @@ var roleHauler = {
                         {
                             creep.memory.withdraw = true;
                         }
+                        return;
+                    }
+                }
+
+                // If we're here, all the spawns, extensions and structures are at their capacity. Start moving things to our storage.
+                const storages = creep.room.find(FIND_MY_STRUCTURES,
+                    {
+                        filter: (structure) =>
+                        {
+                            return structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                        }
+                    });
+
+                if (storages.length > 0)
+                {
+                    const target = creep.pos.findClosestByPath(storages);
+                    const transferResult = creep.transfer(target, RESOURCE_ENERGY);
+                    if(transferResult === ERR_NOT_IN_RANGE)
+                    {
+                        if (!creep.fatigue)
+                            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+
+                    if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0)
+                    {
+                        creep.memory.withdraw = true;
                     }
                 }
                 else
                 {
-                    // If we're here, all the spawns, extensions and structures are at their capacity. Start moving things to our storage.
-                    const storages = creep.room.find(FIND_MY_STRUCTURES,
-                        {
-                            filter: (structure) =>
-                            {
-                                return structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                            }
-                        });
-
-                    if (storages.length > 0)
-                    {
-                        const target = creep.pos.findClosestByPath(storages);
-                        const transferResult = creep.transfer(target, RESOURCE_ENERGY);
-                        if(transferResult === ERR_NOT_IN_RANGE)
-                        {
-                            if (!creep.fatigue)
-                                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                        }
-
-                        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0)
-                        {
-                            creep.memory.withdraw = true;
-                        }
-                    }
-                    else
-                    {
-                        // No loitering! - Get out of the way so others can pass.
-                        const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-                        if (!creep.fatigue)
-                            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
+                    // No loitering! - Get out of the way so others can pass.
+                    const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+                    if (!creep.fatigue)
+                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }